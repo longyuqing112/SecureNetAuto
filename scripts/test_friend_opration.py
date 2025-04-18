0bb37c936018a20e7e246d3282f5a772166d4faf
longyuqing112 <2631918708@qq.com>
2025-04-18 11:43:15 +0800

    添加好友/防止首次运行找不到环境元素

--- scripts/test_friend_opration.py
+++ scripts/test_friend_opration.py
@@ -1,8 +1,19 @@
 from pages.windows.friend_operation_page import FriendOperationPage
 
 
-def test_friend_operation(driver):
+def test_delete_friend_operation(driver):
     friend_operation_page = FriendOperationPage(driver)
     friend_operation_page.delete_friend(
         phone='LYQ003'
+    )
+
+def test_add_friend_operation(driver):
+    friend_operation_page = FriendOperationPage(driver)
+    friend_operation_page.add_via_menu (
+        phone='hhhhhhh94'
+    )
+def test_add_friend_via_global_operation(driver):
+    friend_operation_page = FriendOperationPage(driver)
+    friend_operation_page.add_via_global_search (
+        phone='hhhhhhh94'
     )
